f7ef236c4d190b3cee084ad0cf5efb17f80a8c2e
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2021-05-12 17:21:03 +0700

    Initial commit.

---
+++ 01. Permulaan/index.js
@@ -0,0 +1,8 @@
+/* Print the following statements. */
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
