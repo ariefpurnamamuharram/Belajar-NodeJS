303a3940b40d08888a485be26a1c1ca1f9dcd947
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2021-05-22 19:13:03 +0700

    Belajar hari Sabtu, 22 Mei 2021.

---
+++ 04. Belajar promise/ChainingPromises.js
@@ -0,0 +1,90 @@
+const state = {
+    stock: {
+        coffeeBeans: 250,
+        water: 1000
+    },
+    isCofeeMachineBusy: false
+};
+
+const checkAvailability = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!state.isCofeeMachineBusy) {
+                resolve("Mesin kopi siap digunakan.");
+            } else {
+                reject("Maaf, mesin sedang sibuk.");
+            }
+        }, 1000);
+    });
+};
+
+const checkStock = () => {
+    return new Promise((resolve, reject) => {
+        state.isCoffeeMachineBusy = true;
+        setTimeout(() => {
+            if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+                resolve("Stok cukup. Bisa membuat kopi.");
+            } else {
+                reject("Stok tidak cukup!");
+            }
+        }, 1500);
+    });
+};
+
+const brewCoffee = () => {
+    console.log("Membuat kopi Anda...");
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Kopi sudah siap!");
+        }, 2000);
+    });
+};
+
+const boilWater = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Memanaskan air...");
+
+        setTimeout(() => {
+            resolve("Air panas sudah siap!");
+        }, 2000);
+    });
+};
+
+const grindCoffeeBeans = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Menggiling biji kopi...");
+
+        setTimeout(() => {
+            resolve("Kopi sudah siap!");
+        }, 1000);
+    });
+};
+
+function makeEspresso() {
+    checkAvailability()
+        .then((value) => {
+            console.log(value);
+            return checkStock();
+        })
+        .then(value => {
+            console.log(value);
+            const promises = [boilWater(), grindCoffeeBeans()];
+
+            return Promise.all(promises);
+        })
+        .then((value) => {
+            console.log(value);
+            return brewCoffee();
+        })
+        .then((value) => {
+            console.log(value);
+            state.isCofeeMachineBusy = false;
+        })
+        .catch(rejectedReason => {
+            console.log(rejectedReason);
+            state.isCofeeMachineBusy = false;
+        });
+}
+
+makeEspresso();
